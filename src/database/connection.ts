e54835224cace5183742f01605024b792675ed57
gustavo martins <luizgustams@gmail.com>
2026-06-21 01:11:01 -0300

    infra: adjusts database connection path in the root

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,7 +3,7 @@ import sqlite3 from 'sqlite3';
 
 export async function getDatabaseConnection() {
 	return await open({
-		filename: './src/database/poems.db',
+		filename: 'poems.db',
 		driver: sqlite3.Database,
 	});
 }

f72217fdb027a22fc66e46dcc1aa7b3adc74657f
gustavo martins <luizgustams@gmail.com>
2026-06-21 14:02:05 -0300

    feat: Create the post function for creating poems.

--- src/repositories/poemsRepository.ts
+++ src/repositories/poemsRepository.ts
@@ -2,8 +2,26 @@ import { getDatabaseConnection } from '../database/connection.ts';
 
 export async function listAllPoems() {
 	const db = await getDatabaseConnection();
-
 	const poems = await db.all('SELECT * FROM poems');
-
 	return poems;
+}
+
+export async function createPoem(
+	title: string,
+	text: string,
+	author: string,
+	category: string,
+	subcategory: string,
+) {
+	const db = await getDatabaseConnection();
+
+	const stmt = await db.prepare(
+		'INSERT INTO poems (title, text, author, category, subcategory) VALUES (?, ?, ?, ?, ?)',
+	);
+
+	await stmt.run(title, text, author, category, subcategory);
+
+	await stmt.finalize();
+
+	return { message: 'Poem successfully created' };
 }

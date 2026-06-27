7ff243e994f251787b02074e51ad3ccad9a05071
gustavo martins <luizgustams@gmail.com>
2026-06-27 14:15:12 -0300

    feat(routes): Create routes to update and delete books

--- src/routes/library.route.ts
+++ src/routes/library.route.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import {
 	createItemLibrary,
+	deleteItemLibrary,
 	listLibrary,
+	updateItemLibrary,
 } from './../controllers/library.controller.ts';
 
 const router = Router();
 
 router.get('/', listLibrary);
 router.post('/', createItemLibrary);
+router.put('/library', updateItemLibrary);
+router.delete('/library', deleteItemLibrary);
 
 export default router;

8ec341cb9bb69248c3c5adac20f827e8e2512c77
gustavo martins <luizgustams@gmail.com>
2026-06-27 10:53:02 -0300

    chore: Delete redundant folders and files at the current stage of the project.

--- src/database/connection.ts
+++
@@ -1,9 +0,0 @@
-import { open } from 'sqlite';
-import sqlite3 from 'sqlite3';
-
-export async function getDatabaseConnection() {
-	return await open({
-		filename: 'poems.db',
-		driver: sqlite3.Database,
-	});
-}
